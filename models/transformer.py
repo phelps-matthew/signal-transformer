dc022fb58da5557530ee6be750bea49d603897ac
mphelps <matthewphelps@odysseyconsult.com>
2022-05-17 20:39:20 -1000

    add cleaned up txfrmr

--- models/transformer.py
+++ models/transformer.py
@@ -1,11 +1,25 @@
 import copy
+from typing import Optional
 
 import numpy as np
 import torch
 from torch import nn
 
+# TODO: These masking procedures seem unecessarily convoluted and slow, perhaps can simplify?
+
 
 def _make_span_from_seeds(seeds, span, total=None):
+    """Helper function for `_make_mask`. Create mask index array by masking selected
+    indices (seeds) up to contiguous spans.
+
+    Args:
+        seeds: selected indices to apply contiguous masking
+        span: span of a contiguous mask
+        total: last index by which masking is to be applied (last_index = total - 1)
+
+    Returns:
+        mask index array, np.int32 of shape
+    """
     inds = list()
     for seed in seeds:
         for i in range(seed, seed + span):
@@ -16,18 +30,28 @@ def _make_span_from_seeds(seeds, span, total=None):
     return np.array(inds)
 
 
-def _make_mask(shape, p, total, span, allow_no_inds=False):
-    # num_mask_spans = np.sum(np.random.rand(total) < p)
-    # num_mask_spans = int(p * total)
-    mask = torch.zeros(shape, requires_grad=False, dtype=torch.bool)
+# TODO: Appears to be inverse-masking contiguous arrays. Issue?
 
+
+def _make_mask(shape, p, total, span, allow_no_inds=False):
+    """Create mask of contiguous sub-masks.
+
+    Args:
+        shape: shape of mask array, only supported tensors of dims=2
+        p: probability of masking contiguous sequence of span `span`
+        total: last index by which masking is to be applied
+        span: span of a contiguous mask
+        allow_no_inds: permit returning array with no net masking
+
+    Returns:
+        mask tensor, torch.bool of shape (shape)
+    """
+    mask = torch.zeros(shape, requires_grad=False, dtype=torch.bool)
     for i in range(shape[0]):
         mask_seeds = list()
         while not allow_no_inds and len(mask_seeds) == 0 and p > 0:
             mask_seeds = np.nonzero(np.random.rand(total) < p)[0]
-
         mask[i, _make_span_from_seeds(mask_seeds, span, total=total)] = True
-
     return mask
 
 
@@ -43,20 +67,20 @@ class Permute(nn.Module):
 class Transformer(nn.Module):
     def __init__(
         self,
-        in_features,
-        hidden_feedforward=3076,
-        heads=8,
-        layers=8,
-        dropout=0.15,
-        activation="gelu",
-        position_encoder=25,
-        layer_drop=0.0,
-        mask_p_t=0.1,
-        mask_p_c=0.004,
-        mask_t_span=6,
-        mask_c_span=64,
-        start_token=-5,
-        finetuning=False,
+        in_features: int,
+        hidden_feedforward: int = 3076,
+        heads: int = 8,
+        layers: int = 8,
+        dropout: float = 0.15,
+        activation: str = "gelu",
+        position_encoder: int = 25,
+        layer_drop: float = 0.0,
+        mask_p_t: float = 0.1,
+        mask_p_c: float = 0.004,
+        mask_t_span: int = 6,
+        mask_c_span: int = 64,
+        start_token: Optional[int] = -5,
+        finetuning: bool = False,
     ):
         """
         Args:
@@ -68,12 +92,12 @@ class Transformer(nn.Module):
             activation: activate unit in transformer encoder layer
             position_encoder: group conv kernel size in positional encoding; zero to disable
             layer_drop: probability of dropping any individual layer within transformer encoder
-            mask_p_t:
-            mask_p_c:
-            mask_t_span:
-            mask_c_span:
+            mask_p_t: probability of token lying at beginning of contiguous mask
+            mask_p_c: probability of channel lying at beginning of contiguous mask
+            mask_t_span: token contiguous mask span
+            mask_c_span: channel contiguous mask span
             start_token: [CLS] token initialization value
-            finetuning: create finetuning masks in forward function
+            finetuning: apply token and channel masks in forward fn as specified in class args
         """
         super().__init__()
 
@@ -128,19 +152,21 @@ class Transformer(nn.Module):
             [copy.deepcopy(encoder) for _ in range(layers)]
         )
 
-        # initialize replacement vector with 0's
+        # initialize learnable mask token as zeros
         self.mask_replacement = nn.Parameter(
             torch.normal(0, in_features ** (-0.5), size=(in_features,)),
             requires_grad=True,
         )
 
-        # downsample dim = in_features using 1x1 conv
+        # downsample to dim = in_features using 1x1 conv
         self.output_layer = nn.Conv1d(self._transformer_dim, in_features, 1)
 
-        # initialize transforer fc layers according to T-fixup
-        self.apply(self.init_bert_params)
+        # initialize transformer fc layers according to T-fixup
+        self.apply(self.init_linears)
 
-    def init_bert_params(self, module):
+    def init_linears(self, module):
+        """Apply T-fixup initialization scheme to linear layers of transformer encoder
+        block"""
         if isinstance(module, nn.Linear):
             nn.init.xavier_uniform_(module.weight.data)
             if module.bias is not None:
@@ -151,32 +177,36 @@ class Transformer(nn.Module):
             )
 
     def forward(self, x, mask_t=None, mask_c=None):
-        bs, feat, seq = x.shape
+        # batch size, num features, and sequence length
+        N, C, L = x.shape
 
-        if self.training and self.finetuning:
+        if True:
+            # if self.training and self.finetuning:
             if mask_t is None and self.p_t > 0:
-                mask_t = _make_mask((bs, seq), self.p_t, x.shape[-1], self.mask_t_span)
+                mask_t = _make_mask((N, L), self.p_t, total=L, span=self.mask_t_span)
             if mask_c is None and self.p_c > 0:
-                mask_c = _make_mask((bs, feat), self.p_c, x.shape[1], self.mask_c_span)
+                mask_c = _make_mask((N, C), self.p_c, total=C, span=self.mask_c_span)
 
-        # multi-gpu workaround, wastes memory (from official dn3/trainable/layers)
-        x = x.clone()
-
+        # given token mask, fill masked values with learnable masked token
         if mask_t is not None:
             x.transpose(2, 1)[mask_t] = self.mask_replacement
+        # given channel mask, zero out channels
         if mask_c is not None:
             x[mask_c] = 0
 
+        # apply positional encoding and upsample to 3 * C with 1x1 conv
         if self.position_encoder:
             x = x + self.relative_position(x)
         x = self.input_conditioning(x)
 
+        # concat [CLS] (start) token
         if self.start_token is not None:
             in_token = self.start_token * torch.ones((1, 1, 1), requires_grad=True).to(
                 x.device
             ).expand([-1, *x.shape[1:]])
             x = torch.cat([in_token, x], dim=0)
 
+        # forward through transformer with LayerDrop
         for layer in self.transformer_layers:
             if not self.training or torch.rand(1) > self.layer_drop:
                 x = layer(x)
--- notes.md
+++ notes.md
@@ -1,6 +1,8 @@
-## ConvEncoder
-* In group norm paper, given fixed number of 32 groups, He finds that best number of channels per groups n_channels / 2
-* Performance is nearly invariant to group number itself, peaking at 32 groups in ResNets. 
-* GELU weighs input by its sign and magnitude, rather than sign as with RELU. Allows bounded (negative) activation in negative regions. Positive activation is unbounded.
-* GELUS are the expectation of modified Adaptive Dropout. See paper, interesting formulation as the activation function is derived from idea computing expectation of dropout and zoneout of neurons. GELU's appear to perform *much* better than RELU when either are accompanied by dropout.
-* GELU weights its input depending on how much greater it is than other inputs. 
+## Things to investigate
+* conv1d intialization as commented out in BENDER
+* Reinsertion of LayerNorm in TransformerEncoderLayer (no tfixup)
+* Projection head in convencoder
+* Varying kernel filter sizes in convencoder
+** Finding appropriate downsampling factor. Must avoid divergent loss and not simply memorize/overfit
+* Initialization scheme (commmented out in BENDER)
+* Use of x.clone() in official BENDER as workaround for multi-gpu
