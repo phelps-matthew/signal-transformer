15645e613b0638ea3f16ca38c7d7691dc2c41951
phelps-matthew <mgp333@gmail.com>
2022-05-20 20:59:43 -1000

    clean up contrastive, add acc and losses

--- signal_transformer/models/contrastive.py
+++ signal_transformer/models/contrastive.py
@@ -13,22 +13,22 @@ import torch.nn.functional as F
 
 class ContrastiveSSL(nn.Module):
     """
-    Implements contrastive semi-supervised learning using a convencoder and a transformer.
+    Implements contrastive semi-supervised learning. Convencoder generates base feature
+    vectors (tokens) and transformer attention provides contexualization.
+
     Influenced by wav2vec 2.0 and BENDR.
 
     Args:
         convencoder: convolutional encoder model that yield signal representations
         transformer: transformer model applying MHSA to conencoder inputs
-        mask_rate:
-        mask_span:
-        learning_rate:
-        temp: temperature factor in contrastive loss (non-negative)
-        l2_activation_convencoder: factor applied to convencoder regularizer, computed as
-            mean squared activation penalty
+        mask_rate: probability of token lying at beginning of contiguous mask
+        mask_span: token span of a contiguous mask
+        temp: temperature divisor in cosine similarity loss (non-negative)
+        l2_feature_encoder: regularization multiplicand applied to convencoder
+            regularizer, computed as mean squared activation penalty
         multi_gpu: invoke torch.nn.DataParallel
-        unmasked_negative_frac:
-        encoder_grad_frac:
-        num_negatives: number of distractors uniformly sampled from input sequence
+        num_negatives: number of distractors uniformly sampled from feature sequence
+        encoder_grad_frac: gradient supression in convencoder
     """
 
     def __init__(
@@ -40,10 +40,8 @@ class ContrastiveSSL(nn.Module):
         temp: float = 0.5,
         enc_feat_l2: float = 0.001,
         multi_gpu: bool = False,
-        unmasked_negative_frac: float = 0.25,
-        encoder_grad_frac: float = 1.0,
         num_negatives: int = 100,
-        loss_fn: Callable[..., torch.Tensor] = torch.nn.CrossEntropyLoss(),
+        encoder_grad_frac: float = 1.0,
     ):
         super(ContrastiveSSL, self).__init__()
         self._enc_downsample = convencoder.num_encoded
@@ -54,15 +52,15 @@ class ContrastiveSSL(nn.Module):
         self.temp = temp
         self.beta = enc_feat_l2
         self.start_token = getattr(transformer, "start_token", None)
-        self.unmasked_negative_frac = unmasked_negative_frac
         self.num_negatives = num_negatives
-        self.loss_fn = loss_fn
+        self.loss_fn = nn.CrossEntropyLoss()
 
         self.convencoder = convencoder
         self.transformer = transformer
         if multi_gpu:
             self.convencoder = nn.DataParallel(self.convencoder)
             self.transformer = nn.DataParallel(self.transformer)
+        # how to module encoder gradients?
         if encoder_grad_frac < 1:
             convencoder.register_backward_hook(
                 lambda module, in_grad, out_grad: tuple(
@@ -70,9 +68,31 @@ class ContrastiveSSL(nn.Module):
                 )
             )
 
+    def _make_eval_mask(self, shape):
+        """Create evenly spaced mask spans based on half of training mask rate.
+        Ugly, refactor?
+
+        Args:
+            shape: tuple of two elements defining mask shape, (N, L)
+        Returns:
+            mask: torch.float32 (N, L)
+        """
+        N, L = shape
+        mask = torch.zeros((N, L), requires_grad=False, dtype=torch.bool)
+        half_avg_num_seeds = max(1, int(L * self.mask_rate * 0.5))
+        if L <= self.mask_span * half_avg_num_seeds:
+            raise ValueError("Masking the entire span, pointless.")
+        seeds = (L // half_avg_num_seeds) * np.arange(half_avg_num_seeds).astype(int)
+        mask_indices = _make_span_from_seeds(seeds, self.mask_span)
+        mask[:, mask_indices] = True
+        return mask
+
     def info(self, sequence_len):
+        """Return information string regarding mask span, rate, and expectation of total
+        masked elements.
+        """
         encoded_samples = self._enc_downsample(sequence_len)
-        desc = "{} samples | mask span of {} at a rate of {} => E[masked] ~= {}".format(
+        desc = "Samples: {} | Mask span: {} | Mask rate: {} | E[masked] ~= {}".format(
             encoded_samples,
             self.mask_span,
             self.mask_rate,
@@ -80,13 +100,11 @@ class ContrastiveSSL(nn.Module):
         )
         return desc
 
-    @staticmethod
-    def mask_pct(mask):
-        """mask coming from forward output"""
-        return mask.float().mean().item()
-
-    def generate_negatives(self, z):
+    def sample_negatives(self, z):
         """Generate negative samples to compare each sequence location against.
+
+        Args:
+            z: unmasked feature vector sequence (N, C, L)
 
         Returns:
             torch.float32 of shape (N, L, n_negatives, C)
@@ -97,7 +115,7 @@ class ContrastiveSSL(nn.Module):
         # intialize index tensor
         negative_inds = torch.empty(N, L, self.num_negatives).long()
         # create tensor of ones, with zero diagonal. this permits us to exclude the self
-        # token (it is explicitly used in calculate similarity)
+        # token (it is explicitly added in compute_logits)
         ind_weights = torch.ones(L, L) - torch.eye(L)
         # for each batch, randomly sample non-negative elements from rows of ind_weights.
         # we add i * L to get proper indices when we reshape below
@@ -111,8 +129,10 @@ class ContrastiveSSL(nn.Module):
         z_k = z_k[negative_inds.view(-1)].view(N, L, self.num_negatives, C)
         return z_k, negative_inds
 
-    def calculate_similarity(self, z, c, negatives):
+    def compute_logits(self, z, c, negatives):
         """
+        Compute logits of feature vector and distractors using cosine similarity.
+
         Args:
             z: unmasked feature vector sequence (N, C, L)
             c: transformer output feature vector sequence (N, C, L+1)
@@ -125,15 +145,17 @@ class ContrastiveSSL(nn.Module):
         c = c[..., 1:].permute([0, 2, 1]).unsqueeze(-2)
         z = z.permute([0, 2, 1]).unsqueeze(-2)
 
-        # In case the contextualizer matches exactly, need to avoid divide by zero errors
-        # broadcast c, compute equivalence of entire feature vector (dim=-1) matching
+        # if transformer output maches negative distactor exactly, we must explicitly
+        # handle such elements to avoid division by zero, so here we gather such indices
+        # broadcast c, compute potential equivalences of entire feature vectors (dim=-1)
         negative_in_target = (c == negatives).all(-1)
 
-        # add current (self) token to list of distractors, (N, L, n_negatives + 1, C)
+        # include own (self) token within list of distractors, (N, L, n_negatives + 1, C)
         targets = torch.cat([z, negatives], dim=-2)
 
         # compute cosine similarity between c and all distractors, (N, L, n_negatives + 1)
         logits = F.cosine_similarity(c, targets, dim=-1) / self.temp
+
         # ignoring self token, replace all exact matches with -inf so as to not contribute
         # to constastive loss denominator
         if negative_in_target.any():
@@ -143,53 +165,59 @@ class ContrastiveSSL(nn.Module):
 
     @staticmethod
     def contrastive_accuracy(logits):
-        labels = torch.zeros(logits, device=logits.device, dtype=torch.long)
-        # return StandardClassification._simple_accuracy([labels], logits)
-        return labels
+        """
+        Compute accuracy of highest logit scores of transformed features matching
+        encoded feature vectors.
 
-    def calculate_loss(self, logits, unmasked_z):
-        # The 0'th index is the correct position
-        labels = torch.zeros(logits.shape, device=logits.device, dtype=torch.long)
-        # Note that loss_fn here integrates the softmax as per the normal classification
-        # pipeline (leveraging logsumexp)
-        l2_activation_loss = self.beta * unmasked_z.pow(2).mean()
-        return self.loss_fn(logits, labels) + l2_activation_loss
+        Args:
+            logits: cosine similarity outputs, (N * L, n_negatives + 1)
+        Returns:
+            torch.tensor scalar (torch.float32)
+        """
+        targets = torch.zeros(logits.shape[0], device=logits.device, dtype=torch.long)
+        preds = logits.argmax(dim=-1)
+        accuracy = preds.eq(targets).float().mean().item()
+        return accuracy
+
+    def contrastive_loss(self, logits):
+        """Calculate crossentropy + L2 feature activation loss
+
+        Args:
+            logits: cosine similarity outputs, (N * L, n_negatives + 1)
+        Returns:
+            torch.tensor scalar (torch.float32)
+        """
+        # Why use long instead of int?
+        targets = torch.zeros(logits.shape[0], device=logits.device, dtype=torch.long)
+        # CrossEntropyLoss applies log(softmax) internally
+        contrastive_loss = self.loss_fn(logits, targets)
+        return contrastive_loss
+
+    def feature_activation_loss(self, unmasked_z):
+        """Compute L2 feature activation loss (from convencoder)
+
+        Args:
+            unmasked_z: unmasked feature vector sequence, (N, C, L)
+        Returns:
+            torch.tensor scalar (torch.float32)
+        """
+        return self.beta * unmasked_z.pow(2).mean()
 
     def forward(self, x):
         z = self.convencoder(x)
         unmasked_z = z.clone()
-
         N, C, L = z.shape
-
         if self.training:
             mask = make_mask((N, L), self.mask_rate, L, self.mask_span)
         # during eval, use half the mask rate and evenly space masks
         else:
-            mask = torch.zeros((N, L), requires_grad=False, dtype=torch.bool)
-            half_avg_num_seeds = max(1, int(L * self.mask_rate * 0.5))
-            if L <= self.mask_span * half_avg_num_seeds:
-                raise ValueError("Masking the entire span, pointless.")
-            mask[
-                :,
-                _make_span_from_seeds(
-                    (L // half_avg_num_seeds)
-                    * np.arange(half_avg_num_seeds).astype(int),
-                    self.mask_span,
-                ),
-            ] = True
-
+            mask = self._make_eval_mask((N, L))
         c = self.transformer(z, mask)
-
         # generate negative distractors for each transformed token
-        negatives, _ = self.generate_negatives(unmasked_z)
-
-        # calculate cosine similarity between transfored features and 
+        negatives, _ = self.sample_negatives(unmasked_z)
+        # calculate cosine similarity between transfored features and
         # distractors as logits (i.e. inputs into softmax-like loss)
-        logits = self.calculate_similarity(unmasked_z, c, negatives)
-        # fmt: off
-        import ipdb; ipdb.set_trace(context=30)  # noqa
-        # fmt: on
-        self.calculate_loss(logits, unmasked_z)
+        logits = self.compute_logits(unmasked_z, c, negatives)
         return logits, unmasked_z, mask, c
 
 
